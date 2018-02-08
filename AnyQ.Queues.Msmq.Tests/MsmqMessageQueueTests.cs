631377492ebc312214cf9a62c8ab86dccd4c784c
Stephen Collins <s.g.collins@outlook.com>
2018-02-07 22:06:17 -0500

    added initial integration test

---
+++ AnyQ.Queues.Msmq.Tests/MsmqMessageQueueTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnyQ.Queues.Msmq.Tests {
+    [TestClass]
+    public class MsmqMessageQueueTests {
+        private const string queueId = @".\private$\AnyQ.Queues.Msmq.Tests";
+
+        private MsmqMessageQueue _sut;
+
+        [TestInitialize]
+        public void Setup() {
+            System.Messaging.MessageQueue.Delete(queueId);
+            _sut = new MsmqMessageQueue(new QueueCreationOptions {
+                QueueId = queueId,
+                QueueName = "AnyQ.Queues.Msmq Test Queue"
+            }, false);
+        }
+
+        [TestMethod]
+        public void InitializeQueue_Creates_Queue() {
+            Assert.IsTrue(System.Messaging.MessageQueue.Exists(queueId));
+        }
+    }
+}
