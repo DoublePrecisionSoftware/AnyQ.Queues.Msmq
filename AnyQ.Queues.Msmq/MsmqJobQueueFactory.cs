b1ad8447ce0fd9ff0fa2283b0820f799d8180dca
Stephen Collins <s.g.collins@outlook.com>
2018-02-07 22:29:49 -0500

    added XML comments

--- AnyQ.Queues.Msmq/MsmqJobQueueFactory.cs
+++ AnyQ.Queues.Msmq/MsmqJobQueueFactory.cs
@@ -3,12 +3,22 @@ using AnyQ.Jobs;
 using System;
 
 namespace AnyQ.Queues.Msmq {
+    /// <summary>
+    /// Provides a simple abstraction over <see cref="MsmqMessageQueueFactory"/> and
+    /// <see cref="MsmqMessageFactory"/> for passing to a <see cref="JobQueueListener"/>
+    /// </summary>
     public class MsmqJobQueueFactory : IJobQueueFactory {
         private readonly MsmqMessageQueueFactory _queueFactory;
         private readonly MsmqMessageFactory _messageFactory;
         private readonly IPayloadFormatter _payloadFormatter;
         private readonly IRequestSerializer _requestSerializer;
 
+        /// <summary>
+        /// Initialize a new instance of the <see cref="MsmqJobQueueFactory"/>
+        /// </summary>
+        /// <param name="payloadFormatter">Formats object payloads added to queue</param>
+        /// <param name="requestSerializer">Deserializes payload coming from the queue</param>
+        /// <param name="accessControlList">Specifies users and their access to the queue</param>
         public MsmqJobQueueFactory(
             IPayloadFormatter payloadFormatter, 
             IRequestSerializer requestSerializer,
@@ -20,6 +30,10 @@ namespace AnyQ.Queues.Msmq {
             _requestSerializer = requestSerializer;
         }
 
+        /// <summary>
+        /// Creates a new <see cref="JobQueue"/> with the proper dependencies
+        /// </summary>
+        /// <param name="handlerConfiguration">Configuration for the handler handling this queue</param>
         public JobQueue Create(HandlerConfiguration handlerConfiguration) {
             return new JobQueue(_queueFactory, _messageFactory, _payloadFormatter, _requestSerializer, handlerConfiguration);
         }
